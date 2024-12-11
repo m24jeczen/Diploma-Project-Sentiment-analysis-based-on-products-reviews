6529d0eb10806da29fe193fa8df8f923f5d462d2
Michal <michal.iwicki00@gmail.com>
2024-12-11 18:45:56 +0100

    Implemented bussines filtering and creator of local data for products stores and reviews

--- codes/downloader.py
+++ codes/downloader.py
@@ -3,33 +3,29 @@ import gzip
 import os
 import pandas as pd
 from io import BytesIO
-import json, csv
-from codes.parameters import target_directory
+import json
+from codes.parameters import target_directory, categories
+from collections import defaultdict
 
  
 def download_source_data(category):
-
+    if category not in categories:
+        print(f"Category {category} does not exist in available categories.")
+        return 
     current_directory = os.getcwd()
-
-    # Uzyskanie ścieżki dwa katalogi wyżej
     upper_catalog = os.path.abspath(os.path.join(current_directory, ".."))
-
-    # Ścieżka do folderu, który chcesz utworzyć
     target_directory = os.path.join(upper_catalog, "amazon_data")
-
-    # Tworzenie folderu, jeśli nie istnieje
     os.makedirs(target_directory, exist_ok=True)
     reviews_url = "https://datarepo.eng.ucsd.edu/mcauley_group/data/amazon_2023/raw/review_categories/"
     
-    # Nadanie nazw plikom
     reviews_extracted_filename = category + ".jsonl"
     reviews_jsonl_file_path = os.path.join(target_directory, reviews_extracted_filename)
  
     reviews_url = reviews_url +reviews_extracted_filename+ ".gz"
-    # Pobranie pliku z URL i zapisanie go lokalnie
+    print(f"Downloading {category}")
     reviews_response = requests.get(reviews_url)
-    reviews_response.raise_for_status()  # Sprawdź, czy nie wystąpiły błędy podczas pobierania
-   
+    reviews_response.raise_for_status()  
+    
     with gzip.open(BytesIO(reviews_response.content), "rt", encoding="utf-8") as gz_file:
         with open(reviews_jsonl_file_path, "w", encoding="utf-8") as jsonl_file:
             jsonl_file.write(gz_file.read())
@@ -37,26 +33,24 @@ def download_source_data(category):
     print(f"File saved and loaded as {reviews_extracted_filename}")
     
     meta_url = "https://datarepo.eng.ucsd.edu/mcauley_group/data/amazon_2023/raw/meta_categories/"
-   
-    # Nadanie nazw plikom
+
     meta_extracted_filename = "meta_" + category + ".jsonl"
     meta_jsonl_file_path = os.path.join(target_directory, meta_extracted_filename)
  
     meta_url = meta_url +meta_extracted_filename+ ".gz"
-    # Pobranie pliku z URL i zapisanie go lokalnie
     meta_response = requests.get(meta_url)
-    meta_response.raise_for_status()  # Sprawdź, czy nie wystąpiły błędy podczas pobierania
+    meta_response.raise_for_status() 
    
     with gzip.open(BytesIO(meta_response.content), "rt", encoding="utf-8") as gz_file:
         with open(meta_jsonl_file_path, "w", encoding="utf-8") as jsonl_file:
             jsonl_file.write(gz_file.read())
  
     print(f"File saved and loaded as {meta_extracted_filename}")
-    
-def create_local_data(category):
-    # Meta data not used yet maybe will be later
-    meta_path, review_path = os.path.join(target_directory,"meta_" + category+".csv"), os.path.join(target_directory,category+".jsonl")
+
+
+def create_aggregated_data_and_save_reviews_data(category):
     output_file = os.path.join(target_directory,category + ".csv")
+    review_path= os.path.join(target_directory,category+".jsonl")
     selected_columns = ["rating","text", "parent_asin","timestamp"]
     data = []
     with open(review_path, 'r', encoding='utf-8') as review_file:
@@ -64,22 +58,87 @@ def create_local_data(category):
             if line.strip():  
                 try:
                     record = json.loads(line)
-                    # Get selected parameters and load them to local csv
                     selected_params = {field: record.get(field, "") for field in selected_columns}
                     data.append(selected_params)
                 except json.JSONDecodeError as e:
                     print(f"Problem with decoding meta line: {line}")
+
     data = pd.DataFrame(data, columns=selected_columns)
     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
     data.drop_duplicates(inplace=True)
-    aggregated = data.groupby('parent_asin').agg(
-        rating_number=('rating', 'size'),  # Liczba wierszy dla każdej grupy
-        average_rating=('rating', 'mean')  # Średnia wartość rating
-    ).reset_index()
-    data = data.merge(aggregated, on='parent_asin', how='left')
     data.to_csv(output_file, index=False)
+    aggregated = data.groupby('parent_asin').agg(
+        rating_number=('rating', 'size'),  
+        average_rating=('rating', 'mean')  
+    ).reset_index()
+
+    return aggregated
 
 
+def create_store_product_data(category):
+    meta_path = os.path.join(target_directory,"meta_"+category+".jsonl")
+    selected_columns = ["parent_asin", "title", "store"]
+    product_data = []
+    store_data = defaultdict(set)
+    with open(meta_path, 'r', encoding='utf-8') as meta_file:
+        for line in meta_file:
+            if line.strip():  
+                try:
+                    record = json.loads(line)
+                    selected_params = {field: record.get(field, "") for field in selected_columns}
+                    product_data.append(selected_params)
+                    store_data[selected_params["store"]].add(selected_params["parent_asin"])
+                except json.JSONDecodeError as e:
+                    print(f"Problem with decoding meta line: {line}")
+
+    product_data = pd.DataFrame(product_data, columns=selected_columns)
+    store_file = os.path.join(target_directory,"store_"+category+".txt")
+    with open(store_file, mode='w', encoding='utf-8') as outfile:
+        for key, values in store_data.items():
+            outfile.write(f"{key}: {', '.join(values)}\n")
+
+    return product_data
+
+
+    
+def create_local_data(category):
+    if category not in categories:
+        print(f"Category {category} does not exist in available categories.")
+        return 
+    
+    review_path, meta_path = os.path.join(target_directory,category+".jsonl"), os.path.join(target_directory,"meta_"+category+".jsonl")
+
+    if not os.path.exists(review_path) or not os.path.exists(meta_path):
+        print(f"Data {category}  is not available locally. Downloading neccesary files.")
+        download_source_data(category)
+
+    aggregated = create_aggregated_data_and_save_reviews_data(category)
+    product_data = create_store_product_data(category)
+    product_data = product_data.merge(aggregated, left_on='parent_asin', right_on='parent_asin')
+    product_file = os.path.join(target_directory,"product_"+category+".csv")
+    product_data.to_csv(product_file, index=False)
+
+
+def load_store_data(category):
+    review_path, meta_path = os.path.join(target_directory,category+".jsonl"), os.path.join(target_directory,"meta_"+category+".jsonl")
+    if not os.path.exists(review_path) or not os.path.exists(meta_path):
+        print(f"Data {category}  is not available locally. Downloading neccesary files.")
+        download_source_data(category)
+
+    store_file= os.path.join(target_directory,"store_"+category+".txt")
+    if not os.path.exists(store_file):
+        print(f"Store_data for {category}  is not available. Creating neccesary files.")
+        create_local_data(category)
+
+    store_data={}
+    with open(store_file, mode='r', encoding='utf-8') as infile:
+        for line in infile:
+            # Deleting few stores with invalid names, very small sample lost
+            if line.count(":") == 1:
+                key, values = line.strip().split(":")
+                store_data[key.strip()] = set(values.strip().split(", "))
+               
+    return store_data
     
 
 
--- codes/filter.py
+++ codes/filter.py
@@ -2,20 +2,56 @@ import pandas as pd
 from datetime import datetime
 import json, os
 from codes.parameters import target_directory
+from codes.downloader import load_store_data
 
 
 
-def filter(category, min_length, min_reviews_per_product, min_average_rating):
-    data = pd.read_csv(os.path.join(target_directory, f"{category}.csv"))
-    #data['timestamp_date'] = pd.to_datetime(data['timestamp'], unit='ms')     
+def filter(category, min_text_length = None, start_date = None, end_date = None, min_reviews_per_product = None, min_average_rating = None, store = None, search_value = None):
+    product_data = pd.read_csv(os.path.join(target_directory, f"product_{category}.csv")) 
+    review_data = pd.read_csv(os.path.join(target_directory, f"{category}.csv"))  
+    
+    # Filtering with used parameteres. In later version will be implemented more elegant propably with object filter
+    if store is not None:
+        # store_data is a dict with key as a store name and value is set of products of the store
+        store_data = load_store_data(category)
+        if store not in store_data.keys():
+            return f"Store {store} doesn't exist."
+        
+        store_products=store_data[store]
+        product_data = product_data[product_data["parent_asin"].isin(store_products)]
+
+    if search_value is not None:
+        if not isinstance(search_value, str):
+            return f"{search_value} should be a string."
+        product_data=product_data[product_data["title"].str.contains(search_value, case=False, na=False)]
+
+
+    if min_reviews_per_product is not None:
+        if not isinstance(min_reviews_per_product, int) and min_reviews_per_product <= 0:
+            return f"{min_reviews_per_product} should be positivie integer."
+        product_data = product_data[product_data["rating_number"] >= min_reviews_per_product]
+
+    if min_average_rating is not None:
+        if not isinstance(min_average_rating, int) and not (1 <= min_average_rating <= 5):
+            return f"{min_average_rating} should be anumber beetwen 1 and 5"
+        product_data = product_data[product_data["average_rating"] >= min_average_rating] 
+
+    if any(param is not None for param in [min_reviews_per_product, min_average_rating, store, search_value]):
+        review_data = review_data[review_data["parent_asin"].isin(product_data['parent_asin'])]
 
     # time filtring
-    #data = data[(data['timestamp_date'] >= start_date) & (data['timestamp_date'] <= end_date)]
+    if start_date is not None and end_date is not None:
+        try:
+            start_date= pd.to_datetime(start_date)
+            end_date = pd.to_datetime(end_date)
+            review_data['timestamp'] = pd.to_datetime(review_data['timestamp'])
+            review_data = review_data[(review_data['timestamp'] >= start_date) & (review_data['timestamp'] <= end_date)]
+        except ValueError:
+            return "Wrong date format."
     # min text length filtring
-    data = data[data["text"].str.split().str.len() >= min_length]
-    # min reviews per product filter
-    data = data[data["rating_number"] >= min_reviews_per_product]
-    # min reviews per product filter
-    data = data[data["average_rating"] >= min_average_rating]
+    if min_text_length is not None:
+        if not isinstance(min_text_length, int) and min_text_length <= 0:
+            return f"{min_text_length} should be positivie integer."
+        review_data = review_data[review_data["text"].str.split().str.len() >= min_text_length]
 
-    return data
+    return review_data.reset_index(drop=True)
